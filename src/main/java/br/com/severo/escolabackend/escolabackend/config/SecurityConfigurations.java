fd5c8c8983837c6202b3004a06b02db7b729ee0f
Tiago Severo <tiago.estrazulas@sc.senai.br>
2022-10-13 16:00:07 -0300

    Criando Classe SecurityConfigurations no projeto

---
+++ src/main/java/br/com/severo/escolabackend/escolabackend/config/SecurityConfigurations.java
@@ -0,0 +1,9 @@
+package br.com.severo.escolabackend.escolabackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations {
+}
