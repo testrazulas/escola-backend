3ec0cede9263a90584e99ecf004abbaf0590e051
Tiago Severo <tiago.estrazulas@sc.senai.br>
2022-10-13 16:23:15 -0300

    Trabalhando no metodo SecurityFilterChain para habilitar a permissao GET de listar e detalharTurma para todos

--- src/main/java/br/com/severo/escolabackend/escolabackend/config/SecurityConfigurations.java
+++ src/main/java/br/com/severo/escolabackend/escolabackend/config/SecurityConfigurations.java
@@ -1,9 +1,36 @@
 package br.com.severo.escolabackend.escolabackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations {
+
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    //configuracao de autenticacao
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/turmas").permitAll()
+                .antMatchers(HttpMethod.GET, "/turmas/*").permitAll();
+
+        return http.build();
+
+    }
 }
